094c77ce1950a9a3ddfb28a1efa82682025320a8
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-22 15:30:16 +0900

    Update MainActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/MainActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/MainActivity.java
@@ -12,6 +12,9 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.textfield.TextInputLayout;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import android.widget.Toast;
 
