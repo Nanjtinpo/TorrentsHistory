c9c60f322ead8a231fc033683c87d9ef12e39549
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-23 21:31:44 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
@@ -11,7 +11,7 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Element;
+import org.jsoup.nodes.Element;
 import java.io.IOException;
 
 public class ResultActivity extends AppCompatActivity {
