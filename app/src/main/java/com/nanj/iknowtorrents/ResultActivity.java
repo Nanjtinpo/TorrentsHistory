ab5786e9a562a281de10a59ee2f1fac2392cb1b7
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-23 20:29:23 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import java.io.IOException;
 
 public class ResultActivity extends AppCompatActivity {
   @Override
@@ -14,7 +20,27 @@ public class ResultActivity extends AppCompatActivity {
     Intent intent = getIntent();
     String searchip = intent.getStringExtra("searchip");
     
-    TextView textView = (TextView)findViewById(R.id.resulttext);
-    textView.setText(searchip);
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+        .url(searchip)
+        .build();
+    client.newCall(request).enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {}
+      @Override
+      public void onResponse(Call call, Response response) throws IOException {
+        if(!response.isSuccessful()){
+          throw new IOException("Error : " + response);
+        }
+        final String html = response.body().string();
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            TextView textView = (TextView)findViewById(R.id.resulttext);
+            textView.setText(html);
+          }
+        });
+      }
+    });
   }
 }
