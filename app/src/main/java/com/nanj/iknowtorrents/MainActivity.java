17f948c3f251c2a6c2ab036ad64742e38ed10f21
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-23 21:10:55 +0900

    Update MainActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/MainActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/MainActivity.java
@@ -56,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
             runOnUiThread(new Runnable() {
               @Override
               public void run() {
-                TextInputLayout textField = (TextInputLayout)findViewById(R.id.searchip);
-                textField.getEditText().setText("");
                 Intent intent = new Intent(getApplication(), ResultActivity.class);
                 intent.putExtra("searchip", myip);
                 startActivity(intent);
