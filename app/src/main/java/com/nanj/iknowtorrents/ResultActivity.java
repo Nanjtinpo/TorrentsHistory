23089757f49d16f9bddbc6c6e60187d1c678f245
NanJ-Dev <nanj-dev@nekochan.fr>
2020-06-23 19:27:12 +0900

    Remove Fullscreendialog

--- app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
@@ -3,21 +3,15 @@ package com.nanj.iknowtorrents;
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.franmontiel.fullscreendialog.FullScreenDialogFragment;
 
 import android.widget.Toast;
 
-public class ResultActivity extends AppCompatActivity implements FullScreenDialogFragment.OnDiscardListener {
+public class ResultActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // setContentView(R.layout.activity_result);
+    setContentView(R.layout.activity_result);
 
-    new FullScreenDialogFragment.Builder(this)
-                    .setTitle(R.string.app_name)
-                    .setOnDiscardListener(onDiscardListener)
-                    .build();
-    
     Intent intent = getIntent();
     String searchip = intent.getStringExtra("searchip");
     Toast.makeText(this, searchip, Toast.LENGTH_LONG).show();
