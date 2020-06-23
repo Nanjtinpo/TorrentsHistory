70cd22e5d3bab490a4f49857fa64495b668c47d0
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-23 19:04:15 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
@@ -10,8 +10,14 @@ public class ResultActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_result);
+    // setContentView(R.layout.activity_result);
 
+    new FullScreenDialogFragment.Builder(MainActivity.this)
+                    .setTitle(R.string.app_name)
+                    .setOnDiscardListener(onDiscardListener)
+                    .setContent(ContentFragment.class, argumentsBundle)
+                    .build();
+    
     Intent intent = getIntent();
     String searchip = intent.getStringExtra("searchip");
     Toast.makeText(this, searchip, Toast.LENGTH_LONG).show();
