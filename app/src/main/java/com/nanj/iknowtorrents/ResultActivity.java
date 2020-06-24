5cb08b0a72eef8521295a4cc3d32b673dec46a65
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-24 20:37:15 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
+++ app/src/main/java/com/nanj/iknowtorrents/ResultActivity.java
@@ -36,7 +36,7 @@ public class ResultActivity extends AppCompatActivity {
     if (Intent.ACTION_SEND.equals(intent.getAction()) && intent.getType() != null) {
       String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
       if (sharedText != null) {
-        Pattern p = Pattern.compile("(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
+        Pattern p = Pattern.compile("(([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])");
         Matcher m = p.matcher(sharedText);
         if (m.find()){
           temp = m.group();
