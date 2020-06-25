f1a35ff35b54d574000c6224aeb575b8e2859b05
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-25 21:08:45 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/torrentshistory/ResultActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/ResultActivity.java
@@ -12,6 +12,7 @@ import android.view.View.OnClickListener;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.github.javiersantos.appupdater.AppUpdater;
@@ -88,14 +89,14 @@ public class ResultActivity extends AppCompatActivity {
             Elements tbody = doc.select("tbody > tr > td");
             String result = tbody.text();
             if (result.isEmpty()) {
-              LinearLayout linearLayout = (LinearLayout)findViewById(R.id.scrollview);
-              TextView textview = new TextView(this);
+              LinearLayoutCompat linearLayoutCompat = (LinearLayoutCompat)findViewById(R.id.scrollview);
+              TextView textview = new TextView(ResultActivity.this);
               textview.setText(searchip + " はTorrentを使用していません");
               textview.setLayoutParams(new LayoutParams(
                   LayoutParams.WRAP_CONTENT,
                   LayoutParams.WRAP_CONTENT));
               textview.gravity = Gravity.CENTER;
-              linearLayout.addView(textview);
+              linearLayoutCompat.addView(textview);
             } else {
               
             }
