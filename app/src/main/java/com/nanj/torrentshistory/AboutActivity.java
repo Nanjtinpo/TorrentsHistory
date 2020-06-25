2786ef2294d000c12b752b3311f8e92259a22d1f
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-25 17:14:06 +0900

    Update AboutActivity.java

--- app/src/main/java/com/nanj/torrentshistory/AboutActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/AboutActivity.java
@@ -47,7 +47,7 @@ public class AboutActivity extends AppCompatActivity {
           case R.id.menuabout:
             return true;
           case R.id.menuupdate:
-            new AppUpdater(getActivity())
+            new AppUpdater(AboutActivity.this)
                 .setDisplay(Display.DIALOG)
                 .setUpdateFrom(UpdateFrom.GITHUB)
                 .setGitHubUserAndRepo("NanJ-Dev", "TorrentsHistory")
