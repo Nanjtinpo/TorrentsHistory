b1507b1a12e5e6aa792610e07c8f13416157c7bc
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-30 08:43:49 +0900

    Update MainActivity.java

--- app/src/main/java/com/nanj/torrentshistory/MainActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
               String responseJSON = response.body().string();
 	      ObjectMapper objectMapper = new ObjectMapper();
 	      JsonNode jsonNode = objectMapper.readTree(responseJSON);
-	      final String responseIP = jsonNode.get("Answer").get(0).get("name").asText();
+	      final String responseIP = jsonNode.get("Answer").get(0).get("data").asText();
               runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
