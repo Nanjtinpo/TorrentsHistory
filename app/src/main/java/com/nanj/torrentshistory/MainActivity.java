23c0b84479a5d544b7d2e0beb6b31c8fecd08168
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-29 20:46:18 +0900

    Update MainActivity.java

--- app/src/main/java/com/nanj/torrentshistory/MainActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/MainActivity.java
@@ -27,6 +27,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
+import java.util.List<E>;
 
 public class MainActivity extends AppCompatActivity {
   // フィールド変数
