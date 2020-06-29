0810e251b7180e35f971adc7723c6124d965619c
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-06-30 05:56:59 +0900

    Update MainActivity.java

--- app/src/main/java/com/nanj/torrentshistory/MainActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/MainActivity.java
@@ -70,7 +70,8 @@ public class MainActivity extends AppCompatActivity {
 	  String searchHostName = textInputLayout.getEditText().getText().toString();
 	  OkHttpClient client = new OkHttpClient();
           Request request = new Request.Builder()
-            .url("https://dns.google/resolve?name=" + searchHostName + "&type=A")
+            .url("https://cloudflare-dns.com/dns-query?name=" + searchHostName + "&type=A")
+	    .addHeader("accept", "application/dns-json")
             .build();
           client.newCall(request).enqueue(new Callback() {
             @Override
