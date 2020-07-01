c56862a1e5bd9929afc337e9abca2a39365d1893
NanJ-Dev <66310094+NanJ-Dev@users.noreply.github.com>
2020-07-01 20:25:58 +0900

    Update ResultActivity.java

--- app/src/main/java/com/nanj/torrentshistory/ResultActivity.java
+++ app/src/main/java/com/nanj/torrentshistory/ResultActivity.java
@@ -5,6 +5,7 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
